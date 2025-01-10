b9989b40b822b2281f97379ffe7edbabc23a6351
25thGilbertsProblem <99099280+25thGilbertsProblem@users.noreply.github.com>
2025-01-10 18:55:10 +0300

    Добавил новые поля в класс Character, добавил новый enum Gender

--- src/characters/Knopochka.java
+++ src/characters/Knopochka.java
@@ -16,29 +16,30 @@ public class Knopochka extends Character{
     }
 
     public void suggestMoving(FieldObject obj, Time time) {
-
         if (time.isAfter(12)) {
-            if (obj == null) {
-                System.out.print(getName() + ", вообще не была заинтересована");
-                speak("пора ехать дальше, уже " + time + "!");
-            }
-            else {
-                if (random.nextBoolean()) {
-                    System.out.print(getName() + ", которая не особенно интересовалась " + obj.getName());
-                    speak("пора ехать дальше. уже " + time + "!");
-                }
-                else {
-                    System.out.print(getName() + " тоже была заинтересована " + obj.getName());
-                    speak("надо осмотреться, но помни, что пора ехать, уже " + time + "!");
-                }
-            }
+            handleAfternoon(obj, time);
+        } else {
+            handleMorning(time);
         }
-
-        else {
-            System.out.print(getName() + ", проверила часы");
-            speak("пока только " + time + ", ещё рано ехать.");
-        }
-
     }
 
+    // Метод, который будет вызывать фразы Кнопочки, если ещё рано ехать
+    private void handleMorning(Time time) {
+        System.out.print(getName() + ", проверила часы");
+        speak("пока только " + time + ", ещё рано ехать.");
+    }
+
+    // Метод, который будет вызывать фразы Кнопочки, если время для отправки уже настало:
+    private void handleAfternoon(FieldObject obj, Time time) {
+        if (obj == null) {
+            System.out.print(getName() + ", вообще не была заинтересована");
+            speak("пора ехать дальше, уже " + time + "!");
+        } else if (random.nextBoolean()) {
+            System.out.print(getName() + ", которая не особенно интересовалась " + obj.getName());
+            speak("пора ехать дальше. уже " + time + "!");
+        } else {
+            System.out.print(getName() + " тоже была заинтересована " + obj.getName());
+            speak("надо осмотреться, но помни, что пора ехать, уже " + time + "!");
+        }
+    }
 }
